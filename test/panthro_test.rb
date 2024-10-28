2642baaf81ce3c29421dfec1eb7dbf85438ad683
Gaston Ramos <ramos.gaston@gmail.com>
2024-10-28 12:23:01 -0300

    All tests passing

--- test/panthro_test.rb
+++ test/panthro_test.rb
@@ -22,7 +22,7 @@ test 'should not cache api calls' do
 end
 
 test 'should redirect the query' do
-  get '/api/v1/dependencies?gems=sinatra'
+  get '/info/sinatra'
 
   assert last_response.ok?
 end
@@ -31,15 +31,15 @@ test 'should return the same response code got it from rubygems' do
   get '/not-exists'
 
   assert last_response.status == 404
-  assert !File.exists?( "#{ Panthro.path }/not-exists" )
+  assert !File.exist?( "#{ Panthro.path }/not-exists" )
 end
 
 test 'should cache static files' do
-  spec_file_path = '/quick/Marshal.4.8/sinatra-1.4.5.gemspec.rz'
-  assert !File.exists?( "#{ Panthro.path }#{ spec_file_path }" )
+  spec_file_path = '/quick/Marshal.4.8/activerecord-7.2.1.2.gemspec.rz'
+  assert !File.exist?( "#{ Panthro.path }#{ spec_file_path }" )
 
   get spec_file_path
 
   assert last_response.ok?
-  assert File.exists?( "#{ Panthro.path }#{ spec_file_path }" )
+  assert File.exist?( "#{ Panthro.path }#{ spec_file_path }" )
 end
